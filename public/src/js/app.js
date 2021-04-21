d8b483af4ab69fdeac3c755511a95faf53c10f52
Paty Alves <patriciaalves10@gmail.com>
2021-04-21 17:59:15 -0300

    details page

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,68 +1,13 @@
 inputSearch = document.getElementById('search');
 cards = document.getElementById('cards');
+// {
+//     id: 1,
+//     title:"teste 1",
+//     image: "src/images/movie1.png"
+// }
 
+let series = [];
 
-let series = [
-    {
-        id: 1,
-        title:"teste 1",
-        image: "src/images/movie1.png"
-    },
-    {
-        id: 2,
-        title:"teste 2",
-        image: "src/images/movie2.png"
-    },
-    {
-        id: 3,
-        title:"teste 3",
-        image: "src/images/movie3.png"
-    },
-    {
-        id: 4,
-        title:"teste 4",
-        image: "src/images/movie1.png"
-    },
-    {
-        id: 5,
-        title:"teste 5",
-        image: "src/images/movie2.png"
-    },
-    {
-        id: 6,
-        title:"teste 6",
-        image: "src/images/movie3.png"
-    },{
-        id: 7,
-        title:"teste 7",
-        image: "src/images/movie1.png"
-    },
-    {
-        id: 8,
-        title:"teste 8",
-        image: "src/images/movie2.png"
-    },
-    {
-        id: 9,
-        title:"teste 9",
-        image: "src/images/movie3.png"
-    },
-    {
-        id: 10,
-        title:"teste 10",
-        image: "src/images/movie1.png"
-    },
-    {
-        id: 11,
-        title:"teste 11",
-        image: "src/images/movie2.png"
-    },
-    {
-        id: 12,
-        title:"teste 12",
-        image: "src/images/movie3.png"
-    }
-];
 
  clearCards = () => {
     while ( cards.hasChildNodes() ) {
@@ -72,6 +17,18 @@ let series = [
 
   showDetails= (data) =>{
     console.log("showDetails --> ", data);
+  }
+
+
+  emptyCards = () => {
+      let emptyWrapper = document.createElement('div');
+      emptyWrapper.className = 'empty';
+
+      let emptyImage =  document.createElement( 'img' );
+      emptyImage.src= 'src/images/illustration-empty-state.png';
+
+      emptyWrapper.appendChild( emptyImage );
+      cards.appendChild( emptyWrapper );
   }
 
 createCars = (data) =>{
@@ -96,16 +53,23 @@ createCars = (data) =>{
     cards.appendChild( cardWrapper );
 }
 
+
  showCards = () => {
     clearCards();
-    series.forEach( (t, i) => {
-        createCars(t);
-    })
+
+    if(series && series.length > 0){
+        series.forEach( (t, i) => {
+            createCars(t);
+        })
+    } else {
+        emptyCards();
+    }
+    
   
   }
 
   doSearch = (search) =>{
-    fetch( `http://www.omdbapi.com/?apikey=XXX&s=${search}&type=series`)
+    fetch( `http://www.omdbapi.com/?apikey=bb33a52&s=${search}&type=series`)
     .then(  (resp)  => resp.json())
     .then(  ( data ) => {
           console.log( 'Sent data', data );
@@ -133,6 +97,8 @@ const debounce = (func, wait) => {
     const searchValue =  inputSearch.value.trim()
     if(searchValue.length >0 ){
         doSearch(searchValue);
-        // showCards();
     }
   }, 800));
+
+
+showCards();
--- public/src/js/details.js
+++ public/src/js/details.js
@@ -3,13 +3,74 @@ var url = new URL(window.location.href);
 var imdbID = url.searchParams.get("id");
 console.log(imdbID);
 
+time = document.getElementById('time');
+title = document.getElementById('title');
+plot = document.getElementById('plot');
+
+cast = document.getElementById('cast');
+genre = document.getElementById('genre');
+director = document.getElementById('director');
+
+poster = document.getElementById('poster');
+
+rateImdb = document.getElementById('rate-imdb');
+rateValueImdb = document.getElementById('rate-value-imdb');
+
+rateTv= document.getElementById('rate-tv');
+rateValueTv= document.getElementById('rate-value-tv');
+
 let serie = null;
 
+updateDetaisls = () => {
+  time.innerHTML = `${serie.Runtime} - ${serie.Year}`;
+  title.innerHTML = serie.Title;
+  rateValueImdb.innerHTML = `${serie.imdbRating}/10`;
+  rateValueTv.innerHTML = `${(10*(serie.imdbRating/100)*100).toFixed(0)} %`;
+  plot.innerHTML=serie.Plot;
 
-fetch( `http://www.omdbapi.com/?apikey=XXX&i=${imdbID}&type=series`)
+  if(serie.Poster !== "N/A"){
+    poster.src = serie.Poster;
+  }else{
+    poster.src = 'src/images/no-image.png';
+}
+
+  actorsArray = serie.Actors.split(',');
+  genreArray = serie.Genre.split(',');
+  directorArray = serie.Director.split(',');
+
+  actorsArray.forEach(item => {
+    let actorsli = document.createElement('li');
+    actorsli.textContent = item;
+    cast.appendChild(actorsli);
+  });
+
+
+  genreArray.forEach(item => {
+    let genreli = document.createElement('li');
+    genreli.textContent = item;
+    genre.appendChild(genreli);
+  });
+
+
+  directorArray.forEach(item => {
+    let directorli = document.createElement('li');
+    directorli.textContent = item;
+    director.appendChild(directorli);
+  });
+
+  
+
+  // let actorsList = document.createElement('ul');
+
+
+
+}
+
+fetch( `http://www.omdbapi.com/?apikey=bb33a52&i=${imdbID}&type=series`)
     .then(  (resp)  => resp.json())
     .then(  ( data ) => {
           console.log( 'Sent data', data );
         //   console.log( 'Sent results', data.Search );
         serie =   data ;
+        updateDetaisls();
         } )
